76e1f5c04790993cc07d1a282226fdd2bfcb296b
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-30 20:03:20 +0100

    done with the styling, time to code the

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,39 @@
+// import { gsap } from 'gsap';
+
 import './app.scss';
-import { motion } from 'framer-motion';
-import { gsap } from 'gsap';
+
+// importing of assets
+import img1 from '../../assets/1.jpg'
+import img2 from '../../assets/2.jpg'
+import img3 from '../../assets/3.jpg'
+import img4 from '../../assets/4.jpg'
+import img5 from '../../assets/5.jpg'
+import img6 from '../../assets/6.jpg'
+
+const images: string[] = [img1, img2, img3, img4, img5, img6]
 
 const App = () => {
 
     return (
         <div className="AppMain">
-
+            <div className="topBar">STANLEY TO</div>
+            {
+                images.map((echItem, index) => {
+                    console.log(echItem)
+                    return (
+                        <div className="bthCvr" key={index}>
+                            <div className="imgCvr">
+                                <img src={echItem} alt="People" />
+                            </div>
+                            {index < (images.length - 1) && (
+                                <div className="h2Cvr">
+                                    <h2>Skew <span className="outline">Scrolling</span></h2>
+                                </div>
+                            )}
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
